601b0750d6ac9b37ea3f23dd0d6b2d42f3985c19
Jorge Gutiérrez <34327787+Gutima15@users.noreply.github.com>
2023-02-10 21:33:45 -0600

    Adding the project

---
+++ firstProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int numberToGuess = random.Next(1,100);
+            int guess;
+            do{
+                Console.WriteLine("Guess the number between 1 and 100");
+                guess = int.Parse(Console.ReadLine());
+                if(guess < numberToGuess){
+                    Console.WriteLine("Number is greater");
+                } else if ( guess > numberToGuess){
+                    Console.WriteLine("Number is lower");
+                }
+            } while (guess != numberToGuess);
+                Console.WriteLine("You nail it!😊");
+            
+        }
+    }
+}
